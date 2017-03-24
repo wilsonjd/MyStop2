13d4fe18ceffb6761253c1d094c4e01167da31b7
Jeff Wilson <wilsonjd@yahoo.com>
2017-03-24 09:11:58 -0400

    add dismiss button to arrived popup

--- app/src/main/java/us/jeff_wilson/mystop/ArrivedPopupActivity.java
+++ app/src/main/java/us/jeff_wilson/mystop/ArrivedPopupActivity.java
@@ -1,13 +1,37 @@
 package us.jeff_wilson.mystop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 
 public class ArrivedPopupActivity extends AppCompatActivity {
+
+    private static final String TAG = "us.jeff_wilson.mystop";
+
+    Button dismissButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arrived_popup);
+
+        dismissButton = (Button) findViewById(R.id.dismissButton);
+
+        dismissButton.setOnClickListener(new AdapterView.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "dismiss button clicked");
+
+                Intent i=new Intent(ArrivedPopupActivity.this, MainActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+
+                finish();
+            }
+        });
     }
 }
